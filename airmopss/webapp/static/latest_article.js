38bb11826768448201c8358d509fec8ce011df7a
Charles-Elie SIMON <charleselie.simon@gmail.com>
2022-06-23 11:56:42 +0200

    Adding display of who, where...

--- airmopss/webapp/static/latest_article.js
+++ airmopss/webapp/static/latest_article.js
@@ -3,12 +3,18 @@ $(function() {
 
     function maFonction() {
         var event = $(this).data('event');
-        $('#event-container').text(event);
+        event = JSON.parse(event.replace(/'/g, '"'));
+
+        who = event.Who;
+        what = event.What.answer;
+        where = event.Where.answer;
+        when = event.When.answer;
+
+        $('#who').text(who);
+        $('#what').text(what);
+        $('#where').text(where);
+        $('#when').text(when);
     }
     $(".tooltip").click(maFonction);
-
-
-    console.log('LOADED2');
-    console.log('LOADED2');
 });
 
