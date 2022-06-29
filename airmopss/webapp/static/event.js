8371ced414c6fb044ade8ef5aac7d54bca5446a5
Charles-Elie SIMON <charleselie.simon@gmail.com>
2022-06-29 16:55:15 +0200

    Adding labeling feature with Ajax

--- airmopss/webapp/static/event.js
+++ airmopss/webapp/static/event.js
@@ -2,7 +2,10 @@ $(function() {
     console.log($("#event-container"));
 
     function maFonction() {
+        var event_start_idx = $(this).data('start-idx');
+        var event_end_idx = $(this).data('end-idx');
         var event = $(this).data('event');
+
         console.log(event);
 
         who = event.Who;
@@ -14,7 +17,50 @@ $(function() {
         $('#what').text(what);
         $('#where').text(where);
         $('#when').text(when);
+
+        [$("#event-label-good"), $("#event-label-bad")].forEach(function(element) {
+            element.data('start-idx', event_start_idx);
+            element.data('end-idx', event_end_idx);
+            element.data('event', event);
+        });
     }
     $(".event").click(maFonction);
+
+    function labelEvent() {
+        var article_id = $(".article").data('article-id');
+        var event_start_idx = $(this).data('start-idx');
+        var event_end_idx = $(this).data('end-idx');
+        var event = $(this).data('event');
+        var label;
+
+        if ($(this).attr('id') == 'event-label-good') {
+            label = 1;
+        } else {
+            label = 0;
+        }
+
+        labeled_event_data = {
+            article_id: article_id,
+            event_start_idx: event_start_idx,
+            event_end_idx: event_end_idx,
+            event: event,
+            label: label
+        }
+
+        console.log(labeled_event_data);
+
+        $.ajax({
+            type: "POST",
+            url: "/labels",
+            data: JSON.stringify(labeled_event_data),
+            contentType: "application/json",
+            dataType: 'json',
+            success: function(data, _, _) {
+                console.log(data);
+            }
+        });
+    }
+    $("#event-label-good").click(labelEvent);
+    $("#event-label-bad").click(labelEvent);
 });
 
--- airmopss/webapp/webapp.py
+++ airmopss/webapp/webapp.py
@@ -4,6 +4,8 @@
 from flask import Flask, render_template, request
 import argparse
 import json
+import csv
+from os.path import exists
 
 from ..dataloader import DataLoader
 from ..dataprocessing import DataProcessing
@@ -63,7 +65,7 @@ def events():
         events = article_events[article_id]
         events_list = [(event["start_idx"], event["end_idx"], json.dumps(event["details"])) for event in events["events"]]
 
-        return render_template('event.html', article=article, article_len=len(article), events=events, events_list=events_list)
+        return render_template('event.html', article=article, article_id=article_id, article_len=len(article), events=events, events_list=events_list)
 
     else:
         article = request.form['article']
@@ -71,4 +73,28 @@ def events():
         events = qa_processor.get_events(article)
         events_list = [(event["start_idx"], event["end_idx"], json.dumps(event["details"])) for event in events["events"]]
 
-        return render_template('event.html', article=article, article_len=len(article), events=events, events_list=events_list)
+        return render_template('event.html', article=article, article_id=-1, article_len=len(article), events=events, events_list=events_list)
+
+@app.route('/labels', methods=['POST'])
+def labels():
+    labeled_event = request.json
+    csv_row = [
+        labeled_event['article_id'],
+        labeled_event['event_start_idx'],
+        labeled_event['event_end_idx'],
+        labeled_event['event'],
+        labeled_event['label']
+    ]
+    app.logger.debug(csv_row);
+
+    if not exists('labeled_events.csv'):
+        header = ['article_id', 'event_start_idx', 'event_end_idx', 'event', 'label']
+        with open('labeled_events.csv', 'w', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+
+    with open('labeled_events.csv', 'a', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_row)
+
+    return json.dumps({'success': True}), 201, {'ContentType':'application/json'} 
